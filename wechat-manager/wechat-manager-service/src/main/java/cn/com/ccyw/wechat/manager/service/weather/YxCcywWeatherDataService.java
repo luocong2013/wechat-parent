22d0e4eb4cb1048369dd01c55a0a5e88df7bf23b
luocong2013 <luocong2013@outlook.com>
2017-12-21 22:50:12 +0800

    配置数据库连接池

---
+++ wechat-manager/wechat-manager-service/src/main/java/cn/com/ccyw/wechat/manager/service/weather/YxCcywWeatherDataService.java
@@ -0,0 +1,11 @@
+package cn.com.ccyw.wechat.manager.service.weather;
+
+/**
+ * @author luoc
+ * @version V1.0
+ * @package cn.com.ccyw.wechat.manager.service.weather
+ * @description: TODO
+ * @date 2017/12/21 22:28
+ */
+public interface YxCcywWeatherDataService {
+}
